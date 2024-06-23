e949ec5258ba250eb638b67cb46941625b7ee1ef
V-Justin <168036335+V-Justin@users.noreply.github.com>
2024-06-22 23:37:02 -0600

    Script Mod
    Paddle script modified

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
     public float Speed = 2.0f;
     public float MaxMovement = 2.0f;
-    
+    public TextMeshProUGUI playerNameDisplay;
+
+    private Renderer paddleRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleRenderer = GetComponent<Renderer>();
+        if (PlayerSelection.Instance != null)
+        {
+            SetColor(PlayerSelection.Instance.PlayerColor);
+            if (playerNameDisplay != null)
+            {
+                playerNameDisplay.text = PlayerSelection.Instance.PlayerName;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -27,5 +39,13 @@ public class Paddle : MonoBehaviour
             pos.x = -MaxMovement;
 
         transform.position = pos;
+    }
+
+    void SetColor(Color color)
+    {
+        if (paddleRenderer != null)
+        {
+            paddleRenderer.material.color = color;
+        }
     }
 }
