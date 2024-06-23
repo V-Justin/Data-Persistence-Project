c117c2af97d05a6a3d2e589bea5ee8c7538bddea
V-Justin <168036335+V-Justin@users.noreply.github.com>
2024-06-22 23:38:45 -0600

    Script Mods
    changes to scripts to correct errors

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,31 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+using TMPro;
 
 public class MainManager : MonoBehaviour
 {
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
-
     public Text ScoreText;
     public GameObject GameOverText;
-    
+    public Text highScoreText;
+
     private bool m_Started = false;
     private int m_Points;
-    
     private bool m_GameOver = false;
 
-    
-    // Start is called before the first frame update
+    private int m_HighScore; // Track high score
+
     void Start()
     {
+        // Load high score on start
+        LoadHighScore();
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -38,14 +40,14 @@ public class MainManager : MonoBehaviour
         }
     }
 
-    private void Update()
+    void Update()
     {
         if (!m_Started)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
+                float randomDirection = UnityEngine.Random.Range(-1.0f, 1.0f); // Using UnityEngine.Random
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
                 forceDir.Normalize();
 
@@ -59,6 +61,47 @@ public class MainManager : MonoBehaviour
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
+        }
+
+        highScoreText.text = PlayerSelection.Instance.PlayerName + " HIGHSCORE " + m_HighScore;
+
+        // Check for new high score
+        if (m_Points > m_HighScore)
+        {
+            m_HighScore = m_Points;
+            SaveHighScore();
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int HighScore;
+    }
+
+    void SaveHighScore()
+    {
+        SaveData data = new SaveData();
+        data.HighScore = m_HighScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    void LoadHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            m_HighScore = data.HighScore;
+        }
+        else
+        {
+            m_HighScore = 0; // Default high score if file doesn't exist
         }
     }
 
